f3e0eedb8795ea278b42370d263f310010e30d64
Matthew Mann <mjm636@cornell.edu>
2020-06-17 22:23:19 -0400

    Updates

---
+++ precache-manifest.fbe59de43338ee1fdf335e1ca7d09d68.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb237c69566aa7045bfe699029ad0df7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81971c4c926a16d4cbde",
+    "url": "/static/css/main.cc69acb7.chunk.css"
+  },
+  {
+    "revision": "bad75bfd1e6e7d05d353",
+    "url": "/static/js/2.045816b0.chunk.js"
+  },
+  {
+    "revision": "bd186748a279a7c3df1fe1fc35da9637",
+    "url": "/static/js/2.045816b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81971c4c926a16d4cbde",
+    "url": "/static/js/main.4629f7c9.chunk.js"
+  },
+  {
+    "revision": "04e259f1c7b482618350",
+    "url": "/static/js/runtime-main.30ba31c6.js"
+  },
+  {
+    "revision": "fc961db2105016bcd29e2f35b0feeb51",
+    "url": "/static/media/barryHunter.fc961db2.png"
+  },
+  {
+    "revision": "335ed0364eb39c8b3eda9d8e78f582ea",
+    "url": "/static/media/dee9.335ed036.png"
+  },
+  {
+    "revision": "f5dd8f8421d77b8b5be5a26448676e6a",
+    "url": "/static/media/event1.f5dd8f84.png"
+  },
+  {
+    "revision": "51d611115398b2c864b331f66f09b2f2",
+    "url": "/static/media/event2.51d61111.png"
+  },
+  {
+    "revision": "42ea01d95a69df52063eabfd1b1599f9",
+    "url": "/static/media/event3.42ea01d9.png"
+  },
+  {
+    "revision": "c77a187394b191a68538b6a8fcc2476c",
+    "url": "/static/media/event4.c77a1873.png"
+  },
+  {
+    "revision": "1bc3ec8ea436450e13571c3159c80700",
+    "url": "/static/media/event5.1bc3ec8e.png"
+  },
+  {
+    "revision": "57ea94003256b49b0fd376296a5e54f5",
+    "url": "/static/media/event6.57ea9400.png"
+  },
+  {
+    "revision": "76400fca61f65e42ad7705af0cef3c82",
+    "url": "/static/media/event7.76400fca.png"
+  },
+  {
+    "revision": "5f6f83a2fadc00cc03adf65895396e28",
+    "url": "/static/media/event8.5f6f83a2.png"
+  },
+  {
+    "revision": "43cffac7f83504c1d17e26ca0b1e77eb",
+    "url": "/static/media/frankieaura.43cffac7.png"
+  },
+  {
+    "revision": "578a1fc3c2cd49894cff65612f76a0e3",
+    "url": "/static/media/jackhouse.578a1fc3.png"
+  },
+  {
+    "revision": "f86dadc160137f1643220587fcf888cd",
+    "url": "/static/media/joeypsycho.f86dadc1.png"
+  },
+  {
+    "revision": "f392b043880f0c88fa1885f4b6da2a28",
+    "url": "/static/media/johnny+will.f392b043.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9c13179d510cde54c8d1d29ea415411b",
+    "url": "/static/media/logo.9c13179d.png"
+  },
+  {
+    "revision": "faf2189ff5af791453313831723a3e03",
+    "url": "/static/media/prettiBoiLF.faf2189f.png"
+  },
+  {
+    "revision": "445a71ad07bfdae2167d989b93a7087c",
+    "url": "/static/media/puddah.445a71ad.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/infiniteyouth/precache-manifest.382b744573df07a4a83e5ab1932dc7cc.js"
+  "/precache-manifest.fbe59de43338ee1fdf335e1ca7d09d68.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/infiniteyouth/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
