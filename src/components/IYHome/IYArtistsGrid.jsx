78309d1cc0294b223578059c9e8c644102bf46d6
Rohan Rai <329rrai@gmail.com>
2021-04-27 20:14:54 -0400

    finished new artist grid

--- src/components/IYHome/IYArtistsGrid.jsx
+++ src/components/IYHome/IYArtistsGrid.jsx
@@ -4,44 +4,49 @@ import { Carousel, Container, Row, Col } from "react-bootstrap";
 
 function IYArtistsGrid(props){
     let artistImages = props.artists.map(artist => {
-        const artistImage = (
-            <img
-              src={require(`./photos/${artist.path}`)}
-              style={{ width: "100%" }}
-              alt=""
-            />
-          );
         return (
-            <div>
-                {artistImage}
-            </div>
+            artistGridElement(artist)
         )
     });
-    
-    console.log(artistImages);
+    let splitArtistImages = [];
+    for (let i = 3; i > 0; i--) {
+        splitArtistImages.push(artistImages.splice(0, Math.ceil(artistImages.length / i)));
+    }
+
+    console.log(splitArtistImages);
     return (
         <Container style={{ padding: "120px 0px" }}>
             <Row>
                 <Col md={1} xs={1}></Col>
-                <Col>
-                    {props.artists.map(artist => {
-                        const artistImage = (
-                            <img
-                              src={require(`./photos/${artist.path}`)}
-                              style={{ width: "100%" }}
-                              alt=""
-                            />
-                          );
-                        return (
-                            <div>
-                                {artistImage}
-                            </div>
-                        )
-                    })}
+                <Col className="artistGrid" >
+                    {splitArtistImages[0].map(component => component)}
+                </Col>
+                <Col className="artistGrid artistGridCenter">
+                    {splitArtistImages[1].map(component => component)}
+                </Col>
+                <Col className="artistGrid">
+                    {splitArtistImages[2].map(component => component)}
                 </Col>
                 <Col md={1} xs={1}></Col>
             </Row>
         </Container>
+    );
+}
+
+
+function artistGridElement(artist){
+    return (
+        <div class="artistGridElement">
+            <a href={artist.link}>
+                <img
+                src={require(`./photos/${artist.path}`)}
+                style={{ width: "100%" }}
+                alt=""
+                href={artist.link}
+                />
+                <text>Socials</text>
+            </a>
+        </div>
     );
 }
 
